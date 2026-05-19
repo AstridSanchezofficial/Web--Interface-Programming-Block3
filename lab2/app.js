664db8b042adf7f04d687bc43f40d2769144dfae
astrid.Sanchez <omid.panahi@vaniercollege.qc.ca>
2026-05-19 19:44:32 -0400

    styling HTML with bootstrap

--- lab2/app.js
+++ lab2/app.js
@@ -2,14 +2,16 @@ const LoadUsersBtn=document.getElementById("load-users")
 const ClearBtn=document.getElementById("clear-button")
 let UsersContainer=document.getElementById("users-container")
 let statusUsers=document.getElementById("status-users")
-
+let Spinner=document.getElementById("spinner")
 LoadUsers()
 ClearUsers()
 function LoadUsers(){
     LoadUsersBtn.addEventListener("click",()=>{
         fetch("https://jsonplaceholder.typicode.com/users")
         .then((response)=>{
+            Spinner.classList.remove("d-none")
             statusUsers.textContent="Loading.."
+            
             if (!response.ok){
                 throw new Error ("We could not load the users this time,try again")
             }
@@ -23,15 +25,17 @@ function LoadUsers(){
                 let users=data
                
                 statusUsers.textContent="Users loaded succesfully!!"
+                Spinner.classList.add("d-none")
 
                 users.slice(0,5).forEach((user)=> {
                     UsersContainer.innerHTML +=`
-                    <div id="user">
+                    <div id="user" class="container  p-4 mx-3 bg-body-tertiary border border-primary-subtle rounded">
                         <p><strong>Name: </strong>${user.name}</p>
                         <p><strong>Email: </strong>${user.email}</p>
                         <p><strong>Phone: </strong>${user.phone}</p>
                         <p><strong>City: </strong>${user.address.city}</p>
                         <p><strong>Company Name:</strong>${user.company.name}</p>
+                        <button class="btn btn-info text-light">Load posts</button>
                     </div>
                     `
                 })
@@ -41,6 +45,7 @@ function LoadUsers(){
         })
         .catch(error=>{
             statusUsers.textContent="We could not access to the API, please check your connection!"
+            Spinner.classList.add("d-none")
         })
 
         
