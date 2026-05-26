be37cf864cdabb7555e633e813005af07e7265fe
astrid.Sanchez <omid.panahi@vaniercollege.qc.ca>
2026-05-25 20:55:43 -0400

    Fetchin data

---
+++ class09/api.js
@@ -0,0 +1,11 @@
+export function fetchUsers(){
+    return fetch("https://jsonplaceholder.typicode.com/user").then(
+        (response) => {
+            if (!response.ok){
+                throw new Error (`HTTP error ${response.status}`); 
+            }
+
+            return response.json();
+        },
+    );
+}
